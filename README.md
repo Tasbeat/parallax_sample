3954c0235dd927f4ba523675e1e9436f70b1840d
Ali Alirahimi <alialirahimi642@gmail.com>
2023-04-10 23:33:40 +0330

    placing parallax sample project

---
+++ README.md
@@ -0,0 +1,12 @@
+# parallax_sensors_bg_example
+
+Demo of the library parallax_sensors_bg
+
+## Getting Started
+
+Clone the repository then go to the directory and run:
+ ```dart 
+1. cd ./example
+2. flutter run
+ ```
+ 
---
+++ lib/main.dart
@@ -0,0 +1,31 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+import 'example1.dart';
+import 'example2.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setEnabledSystemUIMode(SystemUiMode.manual,
+      overlays: [SystemUiOverlay.bottom]);
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp])
+      .then((_) {
+    runApp(const MyApp());
+  });
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const Example1(),
+      //const Example2(),
+    );
+  }
+}
